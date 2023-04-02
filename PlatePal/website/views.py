8a9501305128262f11ab120420b33882ee4bd184
Avi Patel <89864019+apate222@users.noreply.github.com>
2023-04-01 23:25:36 -0400

    Update views.py

--- PlatePal/website/views.py
+++ PlatePal/website/views.py
@@ -9,20 +9,14 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    #if request.method == 'POST': 
-        #note = request.form.get('note')#Gets the note from the HTML 
-
-        #if len(note) < 1:
-        #    flash('Note is too short!', category='error') 
-        #else:
-        #    new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-        #    db.session.add(new_note) #adding the note to the database 
-        #    db.session.commit()
-        #    flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
 
 @views.route('/user', methods=['GET', 'POST'])
 @login_required
 def user():
     return render_template("user.html", user=current_user)
+
+@views.route('/about', methods=['GET', 'POST'])
+@login_required
+def about():
+    return render_template("about.html", user=current_user)
